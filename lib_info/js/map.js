31507fccd6775522abe36dc732059ce11f660254
hyennin <chm050202@gmail.com>
2022-12-01 23:17:40 +0900

    test

--- lib_info/js/map.js
+++ lib_info/js/map.js
@@ -14,35 +14,17 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 // 지도를 생성합니다    
 var map = new kakao.maps.Map(mapContainer, mapOption); 
 
-// 주소-좌표 변환 객체를 생성합니다
-var geocoder = new kakao.maps.services.Geocoder();
+// 결과값으로 받은 위치를 마커로 표시합니다
+var marker = new kakao.maps.Marker({
+    map: map,
+    position: new kakao.maps.LatLng(37.4956786590374, 127.071912029293)
+});
 
-// 주소로 좌표를 검색합니다
-geocoder.addressSearch('서울 강남구 남부순환로 3104', function(result, status) {
+// 인포윈도우로 장소에 대한 설명을 표시합니다
+var infowindow = new kakao.maps.InfoWindow({
+    content: '<div style="width:150px;text-align:center;padding:6px 0;">SETEC</div>'
+});
+infowindow.open(map, marker);
 
-    // 정상적으로 검색이 완료됐으면 
-     if (status === kakao.maps.services.Status.OK) {
-        console.log("검색 성공")
-        console.log(result)
-
-        var coords = new kakao.maps.LatLng(result[0].x, result[0].y);
-        console.log(coords)
-
-        // 결과값으로 받은 위치를 마커로 표시합니다
-        var marker = new kakao.maps.Marker({
-            map: map,
-            position: coords
-        });
-
-        // 인포윈도우로 장소에 대한 설명을 표시합니다
-        var infowindow = new kakao.maps.InfoWindow({
-            content: '<div style="width:150px;text-align:center;padding:6px 0;">SETEC</div>'
-        });
-        infowindow.open(map, marker);
-
-        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-        map.setCenter(coords);
-    } else {
-        console.log("검색 실패")
-    }
-});    
+// 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+map.setCenter(coords);
