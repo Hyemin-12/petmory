7e66d57410e212ff4a5fe6848d5e8952ca22ad09
hyewwonn <w2106@e-mirim.hs.kr>
2022-12-04 01:54:36 +0900

    수정

--- lib_login/js/myPage.js
+++ lib_login/js/myPage.js
@@ -5,7 +5,12 @@ const $id = document.querySelector('.myPage-user-id');
 const $co = document.querySelector('.myPage-user-comment');
 
 function Edit() {
-    $el.innerHTML = `<div class="edt-nickname-container"><p>닉네임</p><input id="edt-nickname" type="text" placeholder="닉네임을 입력하세요." /></div><div class="edt-id-container"><p>아이디</p><input id="edt-id" type="text" placeholder="아이디를 입력하세요." /></div><div class="edt-pw-container"><p>비밀번호</p><input id="edt-pw" type="password" placeholder="비밀번호를 입력하세요." /></div><div class="edt-email-container"><p>이메일</p><input id="edt-email" type="email" placeholder="이메일을 입력하세요." /></div><div class="edt-comment-container"><p>한줄소개</p><input id="edt-comment" type="text" placeholder="소개를 입력하세요." /></div><button id="editok-button" onclick="editOK()">저장</button>`
+    $el.innerHTML = `<div class="edt-nickname-container"><p>닉네임</p><input id="edt-nickname" type="text" placeholder="닉네임을 입력하세요." /></div>
+    <div class="edt-id-container"><p>아이디</p><input id="edt-id" type="text" placeholder="아이디를 입력하세요." /></div>
+    <div class="edt-pw-container"><p>비밀번호</p><input id="edt-pw" type="password" placeholder="비밀번호를 입력하세요." /></div>
+    <div class="edt-email-container"><p>이메일</p><input id="edt-email" type="email" placeholder="이메일을 입력하세요." /></div>
+    <div class="edt-comment-container"><p>한줄소개</p><input id="edt-comment" type="text" placeholder="소개를 입력하세요." />
+    </div><button id="editok-button" onclick="editOK()">저장</button>`
 }
 function editOK() {
     var nv = document.getElementById('edt-nickname').value;
